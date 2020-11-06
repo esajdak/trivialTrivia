3120ded524d73232ebcc2f3918df0eabbcafc4c7
Angel Cruz <curzangel803@gmail.com>
2020-11-05 21:11:50 -0600

    passing tests and returning correct data

--- src/main/java/com/trivialTrivia/api/TriviaApi.java
+++ src/main/java/com/trivialTrivia/api/TriviaApi.java
@@ -1,8 +1,6 @@
 package com.trivialTrivia.api;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trivialTrivia.entity.Category;
@@ -14,9 +12,6 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import org.json.JSONArray;
