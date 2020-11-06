b8c128e4b3dba8c59467e6383a31ac9695ae3fcc
Angel Cruz <curzangel803@gmail.com>
2020-11-05 20:59:07 -0600

    passing tests and returning correct data

--- src/main/java/com/trivialTrivia/api/TriviaApi.java
+++ src/main/java/com/trivialTrivia/api/TriviaApi.java
@@ -2,8 +2,10 @@ package com.trivialTrivia.api;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trivialTrivia.entity.Category;
 import com.trivialTrivia.entity.Trivia;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,79 +15,99 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import util.PropertiesLoader;
 
+/**
+ * The type Trivia api.
+ */
 public class TriviaApi implements PropertiesLoader {
+
     private final Logger logger = LogManager.getLogger(this.getClass());
     protected Properties properties;
 
-    public ArrayList<String> getAllCategories() {
-        ArrayList<String> categories = new ArrayList<>();
+    /**
+     * Gets all categories.
+     *
+     * @return the all categories
+     */
+    public Category[] getAllCategories() {
+        properties = this.loadProperties("/api.properties");
+        String url = properties.getProperty("trivia.url.base") + properties.getProperty("trivia.url.addon.categories.all");
+
+        WebTarget target = ClientBuilder.newClient().target(url);
+
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+
+        //json object
+        JSONArray categoriesJson = responseToJsonArray(response,"trivia_categories");
+
+        Category[] categories = null;
+
+        //try to convert response to category array catch json processing exception
+        try {
+            //map location, location.locationCategories, location.locationAddresses
+            categories = createObjectMapper().readValue(categoriesJson.toString(), Category[].class);
+
+            for (Object trivia : categories) { logger.info(trivia.toString()); }
+        } catch (JsonProcessingException e) {
+            logger.error("Trivia request error:", e);
+        }
+
+        logger.info("size: " + categories.length);
+
         return categories;
-    }
-
-    public ArrayList<String> getAllTypes() {
-        ArrayList<String> types = new ArrayList<>();
-        return types;
-    }
-
-    public ArrayList<String> getAllDifficulties() {
-        ArrayList<String> difficulties = new ArrayList<>();
-        return difficulties;
     }
 
     /**
      * make request to trivia api using category, number of questions and type
      * returns collection of trivia objects
+     *
+     * @param categoryId   the category id
+     * @param questionType the question type
+     * @param difficulty   the difficulty
+     * @param amount       the amount
      * @return array list of trivia objects
      */
-    public void getTrivia(String categoryId, String questionType, String difficulty, String amount) {
+    public Trivia[] getTrivia(String categoryId, String questionType, String difficulty, String amount) {
 
-        String stringRegex = "\\w+";
-
-        WebTarget target = buildRequestTarget(
-            Integer.parseInt(categoryId) > 9 && Integer.parseInt(categoryId) < 33 ? categoryId : null,
-            questionType.matches(stringRegex) ? questionType : null,
-            difficulty.matches(stringRegex) ? difficulty : null,
-            Integer.parseInt(amount) > 0 && Integer.parseInt(amount) < 51 ? amount : null
-        );
+        WebTarget target = buildRequestTarget(categoryId, questionType, difficulty, amount);
 
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
-        logger.info(target);
-        logger.info(response);
-//        //json object
-//        JSONArray businesses = new JSONObject(response).getJSONArray("businesses");
-//
-//        genericDao = new GenericDao(Location.class);
-//
-//        //instantiate and configure object mapper to use java array
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        //configure mapper to use array and not fail on unused properties
-//        mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true)
-//                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//
-//        Location[] locations = null;
-//
-//        //try to convert response to GeoSearch array catch json processing exception
-//        try {
-//            //map location, location.locationCategories, location.locationAddresses
-//            locations = mapper.readValue(businesses.toString(), Location[].class);
-//
-//            //add locations, locationCategories, and locationAdresses to db
-//            Arrays.stream(locations).forEach(genericDao::insert);
-//        } catch (JsonProcessingException e) {
-//            logger.error("GeoSearch request error:", e);
-//        }
-//
-//        return locations;
+
+        //json object
+        JSONArray questions = responseToJsonArray(response,"results");
+
+        Trivia[] triviaSet = null;
+
+        //try to convert response to trivia array catch json processing exception
+        try {
+            //map location, location.locationCategories, location.locationAddresses
+            triviaSet = createObjectMapper().readValue(questions.toString(), Trivia[].class);
+        } catch (JsonProcessingException e) {
+            logger.error("Trivia request error:", e);
+        }
+
+        return triviaSet;
     }
 
+    /**
+     * takes params needed for a trivia request and returns a web target with the params
+     *
+     * @param category category for question
+     * @param questionType type of question answer
+     * @param difficulty difficulty of question
+     * @param amount amount of questions
+     * @return webtarget url
+     */
     private WebTarget buildRequestTarget(String category, String questionType, String difficulty, String amount) {
         properties = this.loadProperties("/api.properties");
-        String url = properties.getProperty("trivia.url.base");
+        String url = properties.getProperty("trivia.url.base") + properties.getProperty("trivia.url.addon.questions");
 
         Client client = ClientBuilder.newClient();
 
@@ -95,4 +117,25 @@ public class TriviaApi implements PropertiesLoader {
             .queryParam(properties.getProperty("trivia.url.addon.type"), questionType)
             .queryParam(properties.getProperty("trivia.url.addon.categories"), category);
     }
+
+    /**
+     * takes string response and collection key value and returns json array
+     * @param response string json
+     * @param collectionKey name of key of collection in json
+     * @return JsonArray
+     */
+    private JSONArray responseToJsonArray(String response, String collectionKey) {
+        return new JSONObject(response).getJSONArray(collectionKey);
+    }
+
+    /**
+     * instnatiate and return object mapper with ignoring uneeded properties by default and using json array for
+     * deserialization
+     * @return object mapper
+     */
+    private ObjectMapper createObjectMapper() {
+        return new ObjectMapper().configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
 }
--- src/main/test/com/trivialTrivia/api/TriviaApiTest.java
+++ src/main/test/com/trivialTrivia/api/TriviaApiTest.java
@@ -1,15 +1,41 @@
 package com.trivialTrivia.api;
 
+import com.trivialTrivia.entity.Trivia;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The type Trivia api test.
+ */
 class TriviaApiTest {
 
+    TriviaApi triviaApi;
+
+    /**
+     * Creating the dao.
+     */
+    @BeforeEach
+    void setUp() {
+        triviaApi = new TriviaApi();
+    }
+
+    /**
+     * Test get trivia.
+     */
     @Test
     void testGetTrivia() {
-        TriviaApi triviaApi = new TriviaApi();
+        Trivia[] triviaSet = triviaApi.getTrivia("", "multiple", "medium", "10");
 
-        triviaApi.getTrivia("9", "multiple", "medium", "10");
+        assertEquals(10, triviaSet.length);
+    }
+
+    /**
+     * Gets all categories.
+     */
+    @Test
+    void getAllCategories() {
+        assertEquals(24, triviaApi.getAllCategories().length);
     }
 }
