73e226a7104da2502c96847de054ffe535a3efcb
GuruKatuwal <gkatuwal@madisoncollege.edu>
2020-11-15 18:56:53 -0600

    trivias file added

--- src/main/java/com/trivialTrivia/persistence/TriviApiDao.java
+++ src/main/java/com/trivialTrivia/persistence/TriviApiDao.java
@@ -1,44 +1,84 @@
 package com.trivialTrivia.persistence;
 
+import com.trivialTrivia.entity.Trivia;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
 
-public class TriviApiDao implements PropertiesLoader {
-
-    private final  Logger logger =LogManager.getLogger(this.getClass());
-    protected  Properties properties;
-
-
-    public Trivia[] getTrivia(String category, String type, String difficulty) {
-
-        properties = this.loadProperties("/api.properties");
-        String url = properties.getProperty("trivia.url.base ");
-
-        Client client = ClientBulider,newClient();
-        WebTarget target = client.target(url).queryParam("category", category).queryParam("difficulty", difficulty).queryParam("type", type);
-
-        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
-
-        //json object
-        JSONArray questions = new JSONObject(response).getJSONArray("questions");
-
-        //instantiate and configure object mapper to use java array
-        ObjectMapper mapper = new ObjectMapper();
-
-        //configure mapper to use array and not fail on unused properties
-        mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import java.util.List;
 
 
-        Trivia[] trivias = null;
+public class TriviApiDao implements TriviaApiInterface<Trivia, String> {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private Session session;
+    private Transaction transaction;
 
-        try {
-            trivias = mapper.readValue(questions.toString(), Trivia[].class);
 
-            Arrays.stream(questions).forEach();
-        }catch(JsonProceesingException e){
-            logger.error("trivia question request error " +  e)
-        }
-        return  questions;
+    public TriviApiDao(){
+
+
+    }
+
+    public Session openSession(){
+        session = getSessionFactory().openSession();
+        return session;
+    }
+
+    public Session openSessionWithTransaction(){
+        session = getSessionFactory().openSession();
+        transaction = session.beginTransaction();
+
+        return session;
+    }
+    private static SessionFactory getSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+        return sessionFactory;
+    }
+    public Session getSession() {
+        return session;
     }
 
 
+
+    @Override
+    public void persist(Trivia entity) {
+        getSession().save(entity);
+    }
+
+    @Override
+    public void update(Trivia entity) {
+        getSession().update(entity);
+    }
+
+    @Override
+    public Trivia findById(String id) {
+        Trivia trivia = (Trivia)getSession().get(Trivia.class, id);
+        return trivia;
+    }
+
+    @Override
+    public void delete(Trivia entity) {
+        getSession().delete(entity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Trivia> findAll() {
+        List<Trivia> triviaList = (List<Trivia>) getSession().createQuery("from " + Trivia).list();
+        return triviaList;
+    }
+
+    @Override
+    public void deleteAll() {
+        List<Trivia> entityList = findAll();
+        for (Trivia entity : entityList){
+            delete(entity);;
+        }
+    }
 }
---
+++ src/main/java/com/trivialTrivia/persistence/Trivias.java
@@ -0,0 +1,36 @@
+package com.trivialTrivia.persistence;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trivialTrivia.api.TriviaApi;
+import com.trivialTrivia.entity.Trivia;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+@Path("/trivias")
+public class Trivias  {
+    private Logger logger = LogManager.getLogger(this.getClass());
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTrivias(@QueryParam("triviaCategory") String category, @QueryParam("triviaAmount") String amount,
+                               @QueryParam("triviaDifficulty") String difficulty, @QueryParam("triviaType") String type) {
+        // instantiate trivia api
+        TriviaApi triviaApi = new TriviaApi();
+        // get trivia
+        Trivia[] trivia = triviaApi.getTrivia(category, type, difficulty, amount);
+        JSONObject response = new JSONObject();
+        if (trivia.length > 0) {
+            response.put("trivia", trivia);
+        } else {
+            response.put("error", "No trivia questions found");
+        }
+        return Response.ok().entity(response).build();
+    }
+}
+}
+
